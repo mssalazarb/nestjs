f9f58df953402b2c9aafc31d5ef456162aad1c12
Michael Salazar <michael.salazar@rappi.com>
2022-06-29 00:32:49 -0500

    Add new service for manage internationalization words and text

---
+++ lang-service/src/Database/1656476750649-Translations.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table, TableIndex } from 'typeorm';
+
+export class Translations1656476750649 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'translations',
+        columns: [
+          {
+            name: 'id',
+            type: 'bigint',
+            isPrimary: true,
+          },
+          {
+            name: 'group',
+            type: 'varchar',
+          },
+          {
+            name: 'lang',
+            type: 'varchar',
+          },
+          {
+            name: 'key_word',
+            type: 'varchar',
+          },
+          {
+            name: 'value',
+            type: 'varchar',
+          },
+        ],
+      }),
+      true,
+    );
+
+    await queryRunner.createIndex(
+      'translations',
+      new TableIndex({
+        name: 'IDX_QUESTION_GROUP',
+        columnNames: ['group', 'key_word'],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('translations');
+  }
+}
--- lang-service/src/app.module.ts
+++ lang-service/src/app.module.ts
@@ -3,23 +3,29 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HealthCheckController } from './Controllers/health-check.controller';
 import { HealthCheckService } from './Services/health-check.service';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
+      imports: [ConfigModule.forRoot()],
+      inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [],
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: ['./dist/Models/*{.ts,.js}'],
+        migrations: ['./dist/Database/*{.ts,.js}'],
         synchronize: true,
+        migrationsRun: true,
       }),
-      inject: [ConfigService],
+      dataSourceFactory: async (options) => {
+        return await new DataSource(options).initialize();
+      },
     }),
   ],
   controllers: [HealthCheckController],
